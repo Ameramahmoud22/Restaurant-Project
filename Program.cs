9b320172f0b1cb0d03cfe5b0e636da6728e4055a
Amera <ameraelbassal552005@gmail.com>
2025-05-31 21:14:29 +0300

    AddDbContext<RestaurantDbContext> in program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
 
+using Microsoft.EntityFrameworkCore;
+using RestaurantSystem.Infrastructure.Data;
+
 namespace RestaurantSystem
 {
     public class Program
@@ -10,6 +13,8 @@ namespace RestaurantSystem
             // Add services to the container.
 
             builder.Services.AddControllers();
+            builder.Services.AddDbContext<RestaurantDbContext>(options =>
+                   options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
--- obj/Debug/net8.0/RestaurantSystem.AssemblyInfo.cs
+++ obj/Debug/net8.0/RestaurantSystem.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RestaurantSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f886590ad1955c8c1a04b1ab9e69416541766285")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a40aaaee739113e344e02db3a87f32e7810b0e53")]
 [assembly: System.Reflection.AssemblyProductAttribute("RestaurantSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RestaurantSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
