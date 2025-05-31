99a1542d4740b02ff964262affc6754b13257676
Amera <ameraelbassal552005@gmail.com>
2025-05-31 22:19:50 +0300

    interface IOrderService

---
+++ Interfaces/IOrderService.cs
@@ -0,0 +1,10 @@
+﻿using RestaurantSystem.Models;
+
+namespace RestaurantSystem.Interfaces
+{
+    public interface IOrderService
+    {
+        Task<Order> CreateOrderAsync(Order order);
+        Task<Order> GetOrderByIdAsync(int id);
+    }
+}
--- obj/Debug/net8.0/RestaurantSystem.AssemblyInfo.cs
+++ obj/Debug/net8.0/RestaurantSystem.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RestaurantSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eda6d46a37e12fed08b0797e86f071603469be30")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c83ca593943a7c75a0c581543174e2d1f1263743")]
 [assembly: System.Reflection.AssemblyProductAttribute("RestaurantSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RestaurantSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
