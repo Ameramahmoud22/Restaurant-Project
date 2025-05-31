501840b181377082dda2c959d5d38aace0e5eb08
Amera <ameraelbassal552005@gmail.com>
2025-05-31 22:44:10 +0300

    • Added the required modifier to all non-nullable reference type properties

--- Models/Order.cs
+++ Models/Order.cs
@@ -6,7 +6,7 @@
         public int CustomerId { get; set; }
         public DateTime OrderDate { get; set; }
         public decimal TotalAmount { get; set; }
-        public Customer Customer { get; set; } 
+        public required Customer Customer { get; set; } 
         public List<OrderItem> OrderItems { get; set; } = new List<OrderItem>();
     }
 }
--- Models/OrderItem.cs
+++ Models/OrderItem.cs
@@ -4,9 +4,9 @@
     {
         public int Id { get; set; }
         public int OrderId { get; set; }
-        public Order Order { get; set; } 
+        public required Order Order { get; set; } 
         public int MenuItemId { get; set; }
-        public MenuItem MenuItem { get; set; } 
+        public required MenuItem MenuItem { get; set; } 
         public int Quantity { get; set; }
         public decimal UnitPrice { get; set; }
     }
