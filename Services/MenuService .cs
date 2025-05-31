5f35a6e01655d98a4d09ec33538adaf8843936d8
Amera <ameraelbassal552005@gmail.com>
2025-05-31 22:24:15 +0300

    create MenuService on services folder

---
+++ Services/MenuService .cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RestaurantSystem.Interfaces;
+using RestaurantSystem.Models;
+using RestaurantSystem.Infrastructure.Data;
+
+namespace RestaurantSystem.Services.Services
+{
+    public class MenuService : IMenuService
+    {
+        private readonly RestaurantDbContext _context;
+
+        public MenuService(RestaurantDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<MenuItem>> GetAllMenuItemsAsync()
+        {
+            return await _context.MenuItems.ToListAsync();
+        }
+
+        public async Task<MenuItem> GetMenuItemByIdAsync(int id)
+        {
+            return await _context.MenuItems.FindAsync(id) ?? throw new Exception("Menu item not found");
+        }
+
+        public async Task AddMenuItemAsync(MenuItem menuItem)
+        {
+            _context.MenuItems.Add(menuItem);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
--- obj/Debug/net8.0/RestaurantSystem.AssemblyInfo.cs
+++ obj/Debug/net8.0/RestaurantSystem.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RestaurantSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c83ca593943a7c75a0c581543174e2d1f1263743")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+99a1542d4740b02ff964262affc6754b13257676")]
 [assembly: System.Reflection.AssemblyProductAttribute("RestaurantSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RestaurantSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
