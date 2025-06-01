716daeac4847936f8b09d5d9ac018d53b2ffab47
Amera <ameraelbassal552005@gmail.com>
2025-06-01 16:06:19 +0300

    Add comments to controller and add it to Explanation.txt file

--- Controllers/MenuController.cs
+++ Controllers/MenuController.cs
@@ -1,15 +1,18 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Azure;
+using Microsoft.AspNetCore.Mvc;
 using RestaurantSystem.Interfaces;
 using RestaurantSystem.Models;
 
 namespace RestaurantSystem.API.Controllers
 {
+
     [Route("api/[controller]")]
     [ApiController]
     public class MenuController : ControllerBase
     {
         private readonly IMenuService _menuService;
 
+        // Constructor injection for the IMenuService
         public MenuController(IMenuService menuService)
         {
             _menuService = menuService;
@@ -18,22 +21,22 @@ namespace RestaurantSystem.API.Controllers
         [HttpGet]
         public async Task<IActionResult> GetAll()
         {
-            var menuItems = await _menuService.GetAllMenuItemsAsync();
-            return Ok(menuItems);
+            var menuItems = await _menuService.GetAllMenuItemsAsync(); // Fetch all menu items asynchronously
+            return Ok(menuItems); // Return 200 OK with the list of menu items
         }
 
         [HttpGet("{id}")]
         public async Task<IActionResult> GetById(int id)
         {
-            var menuItem = await _menuService.GetMenuItemByIdAsync(id);
-            return Ok(menuItem);
+            var menuItem = await _menuService.GetMenuItemByIdAsync(id); // Fetch the menu item by ID
+            return Ok(menuItem);  
         }
 
         [HttpPost]
         public async Task<IActionResult> Create([FromBody] MenuItem menuItem)
         {
             await _menuService.AddMenuItemAsync(menuItem);
-            return CreatedAtAction(nameof(GetById), new { id = menuItem.Id }, menuItem);
+            return CreatedAtAction(nameof(GetById), new { id = menuItem.Id }, menuItem); // Return 201 Created with the location of the new resource (EX: /api/Menu/1) and the item’s data.
         }
     }
 }
