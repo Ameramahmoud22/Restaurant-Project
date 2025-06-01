1abdda5e6504ce5edebd6b3e4a54c4783b209ccd
Amera <ameraelbassal552005@gmail.com>
2025-06-01 15:44:45 +0300

    Add commemts to Middlewares File and add it to Explanation.txt File

--- Middlewares/ExceptionMiddleware.cs
+++ Middlewares/ExceptionMiddleware.cs
@@ -1,14 +1,24 @@
-﻿namespace RestaurantSystem.API.Middleware
+﻿
+// to catch and handle exceptions (errors) that occur during HTTP requests to your API
+
+namespace RestaurantSystem.API.Middleware
 {
+
+    //Purpose: This class is the middleware that catches exceptions during request processing.
     public class ExceptionMiddleware
     {
+        //RequestDelegate is a delegate (a type of function pointer) that represents the next step in the middleware pipeline.
+        //_next is the next middleware or endpoint to handle the HTTP request after this one.
         private readonly RequestDelegate _next;
 
+
+        // Uses (DI) to receive the next middleware in the chain and stores it in _next.
         public ExceptionMiddleware(RequestDelegate next)
         {
             _next = next;
         }
 
+        // InvokeAsync is the method that gets called for each HTTP request.
         public async Task InvokeAsync(HttpContext httpContext)
         {
             try
@@ -19,12 +29,16 @@
             {
                 httpContext.Response.ContentType = "application/json";
                 httpContext.Response.StatusCode = StatusCodes.Status500InternalServerError;
+
+                //to create a JSON object like { "error": "Menu item not found" }.
                 await httpContext.Response.WriteAsync(
                     System.Text.Json.JsonSerializer.Serialize(new { error = ex.Message }));
             }
         }
     }
 
+
+    //Purpose: This is a helper class to make it easy to register the middleware
     public static class ExceptionMiddlewareExtensions
     {
         public static IApplicationBuilder UseExceptionMiddleware(this IApplicationBuilder builder)
