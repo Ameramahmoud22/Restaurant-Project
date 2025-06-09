2c5d7df0a8c5311694abfc93a16a68c5a5b1d5a0
Amera <ameraelbassal552005@gmail.com>
2025-06-09 15:33:50 +0300

    Add  Update and Delete methods for MenuService

--- Services/MenuService .cs
+++ Services/MenuService .cs
@@ -35,5 +35,16 @@ namespace RestaurantSystem.Services.Services
             _context.MenuItems.Add(menuItem);
             await _context.SaveChangesAsync();
         }
+        public async Task UpdateMenuItemAsync(MenuItem menuItem)
+        {
+            _context.MenuItems.Update(menuItem);
+            await _context.SaveChangesAsync();
+        }
+        public async Task DeleteMenuItemAsync(int id)
+        {
+            var menuItem = await GetMenuItemByIdAsync(id);
+            _context.MenuItems.Remove(menuItem);
+            await _context.SaveChangesAsync();
+        }
     }
 }
