cfcebf2e7816babea356a3f0c65fa7133e37c10a
Amera <ameraelbassal552005@gmail.com>
2025-06-09 17:21:14 +0300

    Create ReservationController

---
+++ Controllers/ReservationController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RestaurantSystem.Interfaces;
+using RestaurantSystem.Models;
+
+namespace RestaurantSystem.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReservationController : ControllerBase
+    {
+        private readonly IReservationService _reservationService;
+
+        public ReservationController(IReservationService reservationService)
+        {
+            _reservationService = reservationService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Reserve([FromBody] Reservation reservation)
+        {
+            var result = await _reservationService.ReserveTableAsync(reservation);
+            return CreatedAtAction(nameof(GetById), new { id = result.Id }, result);
+        }
+
+        [HttpGet("customer/{customerId}")]
+        public async Task<IActionResult> GetByCustomer(int customerId)
+        {
+            var reservations = await _reservationService.GetReservationsByCustomerAsync(customerId);
+            return Ok(reservations);
+        }
+
+        [HttpGet("available")]
+        public async Task<IActionResult> GetAvailable([FromQuery] DateTime reservationTime, [FromQuery] int numberOfGuests)
+        {
+            var tables = await _reservationService.GetAvailableTablesAsync(reservationTime, numberOfGuests);
+            return Ok(tables);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Cancel(int id)
+        {
+            var result = await _reservationService.CancelReservationAsync(id);
+            if (!result) return NotFound();
+            return NoContent();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            return Ok();
+        }
+    }
+}
--- obj/Debug/net8.0/RestaurantSystem.AssemblyInfo.cs
+++ obj/Debug/net8.0/RestaurantSystem.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RestaurantSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c644d0d816484f360493271bdcca159314b3868f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f01d758dca232047f830733f1337337790725ce")]
 [assembly: System.Reflection.AssemblyProductAttribute("RestaurantSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RestaurantSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
