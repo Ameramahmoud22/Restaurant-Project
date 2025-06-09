c644d0d816484f360493271bdcca159314b3868f
Amera <ameraelbassal552005@gmail.com>
2025-06-09 17:17:55 +0300

    Add IReservationService  interface

---
+++ Interfaces/IReservationService.cs
@@ -0,0 +1,12 @@
+﻿using RestaurantSystem.Models;
+
+namespace RestaurantSystem.Interfaces
+{
+    public interface IReservationService
+    {
+        Task<Reservation> ReserveTableAsync(Reservation reservation);
+        Task<List<Reservation>> GetReservationsByCustomerAsync(int customerId);
+        Task<List<Table>> GetAvailableTablesAsync(DateTime reservationTime, int numberOfGuests);
+        Task<bool> CancelReservationAsync(int reservationId);
+    }
+}
